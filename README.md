87116c1b1016cca5bb20425e60db9ab1fde52fb8
kanokponth <kanokpon.th@ku.th>
2018-08-10 14:22:39 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Data Structure
+
+by Kanokpon Tonabut 5810450172
+
+This repository contains program for data structure
---
+++ src/main/java/csku/datastructure/ArrayCollection.java
@@ -0,0 +1,28 @@
+package csku.datastructure;
+
+/**
+ * Created by 708 on 8/10/2018.
+ */
+public class ArrayCollection implements Collection {
+
+
+    public void add(Object element) {
+
+    }
+
+    public void remove(Object element) {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public void contains(Object element) {
+
+    }
+
+    public int size() {
+        return 0;
+    }
+}
